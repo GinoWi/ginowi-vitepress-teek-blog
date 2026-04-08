1222107847e98b55d8fbe6fcc24665199c7801e7
Jobin Wei <thisweidm@163.com>
2026-04-08 11:09:01 +0800

    update

--- docs/.vitepress/config/Nav.ts
+++ docs/.vitepress/config/Nav.ts
@@ -9,34 +9,30 @@ export const Nav = [
     link: "/",
   },
   {
-    text: "🖥计算机知识体系",
-    items: [
-        { text: "⚙️计算机基础知识", link: "01.计算机基础知识" },
-        { text: "💬人工智能", link: "02.人工智能技术" },
-        { text: "🌏网络安全", link: "03.网络安全技术"},
-        { text: "💾Java全栈", link: "04.Java全栈开发技术"},
-    ],
+    text: "⚙️计算机基础知识",
+    link: "01.计算机基础知识",
+  },
+  { 
+    text: "💬人工智能", 
+    link: "02.人工智能技术",
+  },
+  { 
+    text: "🌏网络安全", 
+    link: "03.网络安全技术",
+  },
+  { 
+    text: "💾Java全栈", 
+    link: "04.Java全栈开发技术",
   },
   {
-    text: "📐数学",
+    text: "基础学科",
     items: [
-
-    ],
+        { text: "📐数学", link: "05.数学" },
+        { text: "📰英语", link: "06.英语" }
+    ]
   },
   {
-    text: "📰英语",
-    items: [
-
-    ],
-  },
-  {
-    text: "🎹钢琴",
-    items: [
-
-    ],
-  },
-  {
-    text: "🧰工具资源",
+    text: "生活",
     items: [
 
     ],
@@ -45,7 +41,6 @@ export const Nav = [
     text: "📒笔记",
     link: '30.笔记',
   },
-  { text: "👂畅所欲言", link: "/message-area/" },
   {
     text: "👏功能页",
     items: [
